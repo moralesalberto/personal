83ec1cf8134841e89e4ecd1ba60bbc6db4c17d91
moralesalberto <alberto@moralitos.com>
2013-10-21 13:13:35 -0400

    deleting link test

--- features/step_definitions/show_keychain_steps.rb
+++ features/step_definitions/show_keychain_steps.rb
@@ -14,9 +14,17 @@ end
 
 Then(/^the show page should have a link to delete the keychain with the name "(.*?)"$/) do |name|
   keychain = Keychain.where("name = ? ",name).first
-  page.html.should have_selector(:xpath, "//a[@href=\"/keychains/#{keychain.id}\"]")
 
-  possible_delete_link = page.all(:css, "a[data-method=delete]").find {|e| e.text == "Delete"}
-  possible_delete_link['href'].should eq("\/keychains\/#{keychain.id}")
+  # this one is too ambiguous
+  page.html.should have_selector(:xpath, "//a[@href=\"/keychains/#{keychain.id}\"]", :text => "Delete")
+
+  #one way to do this using xpath
+  delete_links_via_xpath = page.all(:xpath, "//a[@href=\"/keychains/#{keychain.id}\"]", :text => "Delete")
+  delete_links_via_xpath.select {|link| link['data-method' == 'delete']}
+  delete_links_via_xpath.size.should > 0
+
+  # another way, using css 
+  delete_links = page.all(:css, "a[data-method=delete]").select {|link| link.text == "Delete" and link['href'] == "\/keychains\/#{keychain.id}"}
+  delete_links.size.should > 0
 
 end
